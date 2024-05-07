2547dbebbe51ceb07f98d05187e2f65286637f42
DaronMcCarth <159165840+DaronMcCarth@users.noreply.github.com>
2024-05-07 14:29:04 +0000

    added stuff in js and index

---
+++ js/game.js
@@ -0,0 +1,11 @@
+/* global Phaser */
+
+const config = {
+  type: Phaser.AUTO,
+  width: 19200,
+  height: 1080,
+  backgroundColor: 0x5f6e7a
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
